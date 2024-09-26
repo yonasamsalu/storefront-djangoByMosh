125ee48543cddfe52d0286460e7c8a2be60304b1
Yonas Amsalu <yonasamsalu75@gmail.com>
2024-09-26 12:03:16 +0300

    Managers and Querysets

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,13 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 def say_hello(request):
-    context= {'name':'Yonas'}
-    return render(request, 'hello.html',context)
+    query_set = Product.objects.all()
+    for product in query_set:
+        print (product) 
+
+        
+    return render(request, 'hello.html',)
 
