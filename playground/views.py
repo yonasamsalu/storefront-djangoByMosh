6ba7c62d12a2832fb00519be919980d8ed8c05dc
Yonas Amsalu <yonasamsalu75@gmail.com>
2024-09-26 14:03:34 +0300

    Referecing fields using F objets

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import Q, F
    # Q is short for query
 from django.http import HttpResponse
 from store.models import Product
@@ -22,12 +22,17 @@ def say_hello(request):
            # it is similar with the above
 
       # producs: inventory < 10 OR unit_price < 20
-    queryset = Product.objects.filter(Q(inventory__lt=10) |Q(unit_price__lt=20) )
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20) )
       # it lists all producs either their inventory values are less than 10 or unit_price are less than 20.
       # '|' is an OR operator
-      
+
+       # producs: inventory < 10 OR unit_price < 20
     queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20) )
                     # '~' is a negative form
+
+        # producs: inventory = unit_price 
+    queryset = Product.objects.filter(inventory = F('unit_price'))
+      # a product having equal inventory and unit_price values
 
     return render(request, 'hello.html' ,{'name': 'Yonas Muche','products' : list(queryset)})
 
