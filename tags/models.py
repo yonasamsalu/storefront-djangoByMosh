fddece0ad4588cdf6985087af76c4c6c453c5618
Yonas Amsalu <yonasamsalu75@gmail.com>
2024-09-29 19:38:18 +0300

    inserting a new class model in to tags app

--- tags/models.py
+++ tags/models.py
@@ -8,8 +8,15 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+        return TaggedItem.objects.select_related('tag').filter(content_type=content_type, id=1)
+       
+
 class TaggedItem(models.Model):
     # what tag applied to what object
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField
