4e28d7194f50103fb3e0b1734c60ab5d4559e4f2
Yonas Amsalu <yonasamsalu75@gmail.com>
2024-09-30 21:55:14 +0300

    Cistomizing the List page

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from . import models
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+
 
 # Register the Order model with the admin site
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
+
