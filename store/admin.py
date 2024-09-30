4065649769ac4ad7be967da83040b4cb85ffe6fb
Yonas Amsalu <yonasamsalu75@gmail.com>
2024-09-30 22:07:39 +0300

    List per page

--- store/admin.py
+++ store/admin.py
@@ -3,8 +3,14 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+
+       # The field that is shown in the admin page
     list_display = ['title', 'unit_price']
+
+       # The list that can be edited
     list_editable = ['unit_price']
+       # numbers of lists shown in the admin page
+    list_per_page = 10
 
 # Register the Order model with the admin site
 admin.site.register(models.Collection)
