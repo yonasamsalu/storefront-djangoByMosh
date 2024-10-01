37c9fb12e939a38c44dd4dd4b4baaa2b2913745f
Yonas Amsalu <yonasamsalu75@gmail.com>
2024-10-01 11:39:29 +0300

    Selecting related Objects

--- store/admin.py
+++ store/admin.py
@@ -1,37 +1,38 @@
 from django.contrib import admin
 from . import models
 
-# @admin.register(models.Product)
-# class ProductAdmin(admin.ModelAdmin):
-
-#        # The field that is shown in the admin page
-#     list_display = ['title', 'unit_price']
-
-#        # The list that can be edited
-#     list_editable = ['unit_price']
-#        # numbers of lists shown in the admin page
-#     list_per_page = 10
-
-# # Register the Order model with the admin site
-# admin.site.register(models.Collection)
+admin.site.register(models.Collection)
 
 @admin.register(models.Product)
-class CustomerAdmin(admin.ModelAdmin):
-   #  list_display = ['first_name', 'last_name', 'membership', 'inventory']
-   #  list_editable = ['membership']
-   #  ordering = ['first_name', 'last_name']
+class ProductAdmin(admin.ModelAdmin):
+
+   #     # The field that is shown in the admin page
+   #  list_display = ['title', 'unit_price']
+
+   #     # The list that can be edited
+   #  list_editable = ['unit_price']
+   #     # numbers of lists shown in the admin page
    #  list_per_page = 10
 
-
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status','collection']
     list_editable = ['unit_price']
     list_per_page = 10
     
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory <10:
-            return 'low'
-        return 'ok'
+            return 'Low'
+        return 'Ok'
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership',]
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
+
             
 
 
