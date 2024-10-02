8f0b3847021f04e99475edbb4acdc64384bea83d
Yonas Amsalu <yonasamsalu75@gmail.com>
2024-10-02 22:46:14 +0300

    Adding search to the list page

--- store/admin.py
+++ store/admin.py
@@ -70,8 +70,10 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership',]
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith','last_name__istartswith']
+         ##  "i" before stattswith is case insensitive
 
 
 @admin.register(models.Order)
